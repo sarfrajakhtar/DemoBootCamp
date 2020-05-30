1e884a75c9fa812aea1c6ec64b3adc5a96890c46
sarfrajakhtar <sarfraj.akhtar@thoughtworks.com>
2020-05-30 15:32:19 +0530

    initial

---
+++ src/test/java/com/juiceShop/test/JuiceShopUITest.java
@@ -0,0 +1,38 @@
+package com.juiceShop.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JuiceShopUITest {
+	WebDriver driver;
+    String email = "sarf@gmail.com";
+    String password = "sarf@gmail.com";
+	@Test
+	public void  registerUserTest(){
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("http://192.168.99.100:3000/#/register");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//*[@id=\"mat-dialog-0\"]/app-welcome-banner/div/div[2]/button[2]")).click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.findElement(By.id("emailControl")).sendKeys(email);
+        driver.findElement(By.id("passwordControl")).sendKeys(password);
+        driver.findElement(By.id("repeatPasswordControl")).sendKeys(password);
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.findElement(By.name("securityQuestion")).click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//span[@class='mat-option-text']")).click();
+        driver.findElement(By.id("securityAnswerControl")).sendKeys(password);
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.findElement(By.id("registerButton")).click();
+	}
+}
